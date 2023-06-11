c81a9135e739597965ee78a6624dfb03e8532abf
UTKARSH SRIVASTAVA <uttkarshsrivastava8@gmail.com>
2023-06-11 10:30:02 +0530

    Utkarsh changes

--- app/src/main/java/com/example/aahar100/about.java
+++ app/src/main/java/com/example/aahar100/about.java
@@ -8,11 +8,13 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class about extends AppCompatActivity {
 
     CardView facebook , twitter ,instagram,version;
+    Button test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,4 +55,5 @@ public class about extends AppCompatActivity {
             }
         });
     }
+
 }
